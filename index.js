82dde4fa2a1e6cbfc14d8234dc2f7c7b9d40c958
ninafer <nina.fer012@gmail.com>
2022-07-21 21:08:42 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let userCalification = Number(prompt('Que puntaje le podrias a la clase de hoy?'))
+
+for (let i = 1; i <= userCalification; i++) {
+    alert('Tu calificacion fue ' + userCalification + ' ,Gracias por elegirnos')
+    break
+
+}
